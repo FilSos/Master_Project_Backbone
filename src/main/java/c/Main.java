cd43e03ef1c9b470b953643acee63538f9bbf09b
DESKTOP-RRPOCO6\Filip Sosnowski <fsosnowski@man.poznan.pl>
2019-06-11 16:08:33 +0200

    -CSV button added -logic added

--- src/main/java/c/Main.java
+++ src/main/java/c/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class Main {
@@ -21,13 +22,24 @@ public class Main {
 //        primaryStage.show();
 //    }
 
-    public void btnAddBaseClick(){
+    public void btnAddBaseClick() throws IOException {
         System.out.println("Dodaj baze");
-
+        System.out.println("BtnEmplClick");
+        Stage primaryStage=new Stage();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("empl.fxml")));
+        primaryStage.setTitle("Employees - Departements Management System");
+        primaryStage.setMaximized(true);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.initModality(Modality.APPLICATION_MODAL);
+        primaryStage.show();
     }
 
     public void btnProgramStartClick() {
         System.out.println("Wystartuj program");
+    }
+
+    public void btnAddCSVClick() {
+        System.out.println("Dodaj plik CSV");
     }
 
 }
