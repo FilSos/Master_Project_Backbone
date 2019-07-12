7533dc13829773fe4f9809c7d2c34bf5719648cc
DESKTOP-RRPOCO6\Filip Sosnowski <fsosnowski@man.poznan.pl>
2019-07-12 10:54:03 +0200

    - tableView colums changed

--- src/main/java/controller/Result.java
+++ src/main/java/controller/Result.java
@@ -29,16 +29,18 @@ public class Result implements Initializable {
 
         TableColumn index = new TableColumn("Numer indeksu");
         TableColumn queryString = new TableColumn("Zapytanie");
+        //TableColumn parsed = new TableColumn("Czy prasowanie się udało");
         TableColumn typos = new TableColumn("Literówki");
         TableColumn result = new TableColumn("Poprawność %");
         resultList.getColumns().addAll(index, queryString, typos, result);
-        List<QueryData> list = mainController.resultList;
-        ObservableList<QueryData> items = FXCollections.observableArrayList(list);
-        index.setCellValueFactory(new PropertyValueFactory<QueryData,String>("identifier"));
-        queryString.setCellValueFactory(new PropertyValueFactory<QueryData,String>("queryString"));
-        typos.setCellValueFactory(new PropertyValueFactory<QueryData,Integer>("typos"));
+        List<QueryData> resultList = mainController.resultList;
+        ObservableList<QueryData> items = FXCollections.observableArrayList(resultList);
+        index.setCellValueFactory(new PropertyValueFactory<QueryData, String>("identifier"));
+        queryString.setCellValueFactory(new PropertyValueFactory<QueryData, String>("queryString"));
+        //parsed.setCellValueFactory();
+        typos.setCellValueFactory(new PropertyValueFactory<QueryData, Integer>("typos"));
         //TODO potrzebuje miec Integera, a nei liste
-        result.setCellValueFactory(new PropertyValueFactory<QueryData,List>("score"));
+        result.setCellValueFactory(new PropertyValueFactory<QueryData,Integer>("score"));
 
         this.resultList.setItems(items);
     }
