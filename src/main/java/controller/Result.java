efe19e73e7501be45574ce15e93ecaa63630fe81
DESKTOP-RRPOCO6\Filip Sosnowski <fsosnowski@man.poznan.pl>
2019-07-12 11:40:47 +0200

    -tableView column added

--- src/main/java/controller/Result.java
+++ src/main/java/controller/Result.java
@@ -1,11 +1,11 @@
 package controller;
 
-import com.sun.javafx.robot.impl.FXRobotHelper;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -29,17 +29,23 @@ public class Result implements Initializable {
 
         TableColumn index = new TableColumn("Numer indeksu");
         TableColumn queryString = new TableColumn("Zapytanie");
-        //TableColumn parsed = new TableColumn("Czy prasowanie się udało");
+        TableColumn <QueryData, Boolean> parsed = new TableColumn("Czy parsowanie się udało");
+        parsed.setCellValueFactory(cellData -> new SimpleBooleanProperty(cellData.getValue().isValid()));
         TableColumn typos = new TableColumn("Literówki");
         TableColumn result = new TableColumn("Poprawność %");
-        resultList.getColumns().addAll(index, queryString, typos, result);
+        resultList.getColumns().addAll(index, queryString,parsed, typos, result);
         List<QueryData> resultList = mainController.resultList;
         ObservableList<QueryData> items = FXCollections.observableArrayList(resultList);
         index.setCellValueFactory(new PropertyValueFactory<QueryData, String>("identifier"));
         queryString.setCellValueFactory(new PropertyValueFactory<QueryData, String>("queryString"));
-        //parsed.setCellValueFactory();
+        parsed.setCellFactory(col -> new TableCell<QueryData, Boolean>() {
+            @Override
+            protected void updateItem(Boolean item, boolean empty) {
+                super.updateItem(item, empty) ;
+                setText(empty ? null : item ? "Tak" : "Nie" );
+            }
+        });
         typos.setCellValueFactory(new PropertyValueFactory<QueryData, Integer>("typos"));
-        //TODO potrzebuje miec Integera, a nei liste
         result.setCellValueFactory(new PropertyValueFactory<QueryData,Integer>("score"));
 
         this.resultList.setItems(items);
