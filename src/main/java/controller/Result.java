83413933ace45c05201d6f21dea002809fa75af5
FilSos <filip.sosnowski88@gmail.com>
2019-07-12 09:24:14 +0200

    - listView changed to tableView

--- src/main/java/controller/Result.java
+++ src/main/java/controller/Result.java
@@ -1,10 +1,14 @@
 package controller;
 
+import com.sun.javafx.robot.impl.FXRobotHelper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import query.QueryData;
 
 import java.net.URL;
@@ -18,14 +22,25 @@ public class Result implements Initializable {
 
 
     @FXML
-    public ListView<String> resultList;
+    public TableView<QueryData> resultList;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        resultList.setEditable(true);
+
+        TableColumn index = new TableColumn("Index number");
+        TableColumn queryString = new TableColumn("Query");
+        TableColumn typos = new TableColumn("Typos");
+        TableColumn result = new TableColumn("Result");
+//        index.setCellValueFactory(new PropertyValueFactory("1"));
+//        queryString.setCellValueFactory(new PropertyValueFactory("2"));
+//        typos.setCellValueFactory(new PropertyValueFactory("3"));
+//        result.setCellValueFactory(new PropertyValueFactory("4"));
+        resultList.getColumns().setAll(index, queryString, typos,result);
         Map<Boolean, List<QueryData>> resultList = mainController.resultList;
-        ObservableList<String> items = FXCollections.observableArrayList();
+        ObservableList<QueryData> items = FXCollections.observableArrayList();
         resultList.forEach((key, value) -> value.forEach(query ->
-                items.addAll(query.getQueryString(),query.getIdentifier(),String.valueOf(query.getTypos()),"Poprawność zapytania: " + (100-(query.getTypos()*5)) + "%")
+                items.setAll(query.getIdentifier(),query.getQueryString(),String.valueOf(query.getTypos()),"Poprawność zapytania: " + (100-(query.getTypos()*5)) + "%")
         ));
 
         this.resultList.setItems(items);
