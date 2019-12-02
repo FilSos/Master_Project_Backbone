67b932dccc9b34dd352c932921373cc5ee6f4d3e
FilSos <filip.sosnowski88@gmail.com>
2019-12-02 18:03:19 +0100

    - tableView and excel extended

--- src/main/java/controller/Result.java
+++ src/main/java/controller/Result.java
@@ -9,6 +9,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import model.CodeFragment;
+import model.FragmentValidationResult;
 import query.QueryData;
 
 import java.net.URL;
@@ -24,9 +26,14 @@ public class Result implements Initializable {
     private static TableColumn matchedColumns = new TableColumn("Zgodność kolumn");
     private static TableColumn matchedTables = new TableColumn("Zgodność tabeli");
     private static TableColumn resultMatchScore = new TableColumn("Zgodność z zapytaniem referencyjnym");
+    private static TableColumn fragments = new TableColumn("Zgodność fragmentów");
+    private static TableColumn fragment = new TableColumn("Fragment");
+    private static TableColumn overlapCoefficient = new TableColumn("Współczynnik przesunięcia");
+    private static TableColumn jaroWinklerSimilarity = new TableColumn("Odległóść Jaro - Winklera");
     private static TableColumn typos = new TableColumn("Literówki");
     private static TableColumn result = new TableColumn("Poprawność %");
     private static ObservableList<QueryData> items = null;
+    private String queryDataName = "fragmentValidationResults";
 
     @FXML
     public TableView<QueryData> resultList;
@@ -35,7 +42,8 @@ public class Result implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         resultList.setEditable(true);
         parsed.setCellValueFactory(cellData -> new SimpleBooleanProperty(cellData.getValue().isValid()));
-        resultList.getColumns().addAll(index, exNumber, queryString, parsed, matchedColumns, matchedTables, resultMatchScore, typos, result);
+        fragments.getColumns().addAll(fragment, overlapCoefficient, jaroWinklerSimilarity);
+        resultList.getColumns().addAll(index, exNumber, queryString, parsed, matchedColumns, matchedTables, resultMatchScore, fragments, typos, result);
         this.resultList.setItems(items);
     }
 
@@ -55,6 +63,18 @@ public class Result implements Initializable {
         matchedColumns.setCellValueFactory(new PropertyValueFactory<QueryData, Integer>("matchedColumns"));
         matchedTables.setCellValueFactory(new PropertyValueFactory<QueryData, Integer>("matchedTables"));
         resultMatchScore.setCellValueFactory(new PropertyValueFactory<QueryData, Integer>("resultMatchScore"));
+        //TODO ogarnac wyciaganie danych z listy FragmentValidationResults
+        for (QueryData data : resultList) {
+            if (data.getFragmentValidationResults().size() != 0) {
+                for (FragmentValidationResult result : data.getFragmentValidationResults()) {
+                    fragment.setCellValueFactory(new PropertyValueFactory<CodeFragment, String>("queryFragment"));
+                    overlapCoefficient.setCellValueFactory(new PropertyValueFactory<FragmentValidationResult, Double>("overlapCoefficient"));
+                    jaroWinklerSimilarity.setCellValueFactory(new PropertyValueFactory<FragmentValidationResult, Double>("jaroWinklerSimilarity"));
+                }
+            }
+
+        }
+
         typos.setCellValueFactory(new PropertyValueFactory<QueryData, Integer>("typos"));
         result.setCellValueFactory(new PropertyValueFactory<QueryData, Integer>("score"));
     }
--- src/main/java/converter/ExcelImport.java
+++ src/main/java/converter/ExcelImport.java
@@ -6,9 +6,13 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import query.QueryData;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,16 +22,34 @@ public class ExcelImport {
 
     private static List<Object[][]> dataRows = null;
 
-    public static void doImport(List<QueryData> resultList) {
+    public static void doImport(List<QueryData> resultList) throws IOException {
+        File dir = new File("/wyniki/");
+        dir.mkdirs();
         dataRows = new ArrayList();
-        String FILE_NAME = "/tmp/" + resultList.hashCode() + ".xlsx";
+        String FILE_NAME = "/wyniki/" + resultList.hashCode() + ".xlsx";
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet("Results");
-        Object[][] columns = {{"Student", "Numer zadania", "Zapytanie", "Czy parsowanie się udało", "Zgodność kolumn", "Zgodność tabeli", "Zgodność z zapytaniem referencyjnym", "Literówki", "Poprawność %"}};
+        Object[][] columns = {{"Student", "Numer zadania", "Zapytanie", "Czy parsowanie się udało", "Zgodność kolumn", "Zgodność tabeli", "Zgodność z zapytaniem referencyjnym", "Zgodność fragmentów - fragment", "Zgodność fragmentów - współczynnik przesunięcia", "Zgodność fragmentów - odległóść Jaro - Winklera", "Literówki", "Poprawność %"}};
         dataRows.add(columns);
         for (QueryData data : resultList) {
-            Object[][] row = {{data.getIdentifier(), String.valueOf(data.getExNumber()), data.getQueryString(), data.isValid() ? "Tak" : "Nie", String.valueOf(data.getMatchedColumns()), String.valueOf(data.getMatchedTables()), String.valueOf(data.getResultMatchScore()), String.valueOf(data.getTypos()), String.valueOf(data.getScore())}};
-            dataRows.add(row);
+            if (!data.getFragmentValidationResults().isEmpty()) {
+                Object[][] row = {{data.getIdentifier(), String.valueOf(data.getExNumber()), data.getQueryString(), data.isValid() ? "Tak" : "Nie",
+                        String.valueOf(data.getMatchedColumns()), String.valueOf(data.getMatchedTables()),
+                        String.valueOf(data.getResultMatchScore()), data.getFragmentValidationResults().get(0).getFragment().getQueryFragment(),
+                        String.valueOf(Math.floor(data.getFragmentValidationResults().get(0).getJaroWinklerSimilarity() * 100) / 100),
+                        String.valueOf(Math.floor(data.getFragmentValidationResults().get(0).getOverlapCoefficient() * 100) / 100),
+                        String.valueOf(data.getTypos()), String.valueOf(data.getScore())}};
+                dataRows.add(row);
+            } else {
+                Object[][] row = {{data.getIdentifier(), String.valueOf(data.getExNumber()), data.getQueryString(), data.isValid() ? "Tak" : "Nie",
+                        String.valueOf(data.getMatchedColumns()), String.valueOf(data.getMatchedTables()),
+                        String.valueOf(data.getResultMatchScore()), "brak fragmentu",
+                        "0",
+                        "0",
+                        String.valueOf(data.getTypos()), String.valueOf(data.getScore())}};
+                dataRows.add(row);
+            }
+
         }
 
 
@@ -51,8 +73,8 @@ public class ExcelImport {
                 }
             }
         }
-        //9 - liczba wskazujaca kolumny, z braku lepszego pomyslu, poki co na sztywno
-        for (int i = 0; i < 9; i++) {
+        //12 - liczba wskazujaca kolumny, z braku lepszego pomyslu, poki co na sztywno
+        for (int i = 0; i < 12; i++) {
             sheet.autoSizeColumn(i);
         }
         try {
