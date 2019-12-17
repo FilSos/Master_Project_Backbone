23e8d953d84508c152c7d48d538a2d935183ebfd
Kheldar <psendrowski@gmail.com>
2019-12-17 20:17:44 +0100

    + added log4j logging.

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,7 +1,17 @@
 package view;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+    private static final String CURRENT_VERSION = "0.9";
+    private static final String LAST_CHANGE_DATE = "17.12.2019 20:30";
+    private static Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.info("STARTING APPLICATION Version: " + CURRENT_VERSION);
+        logger.info("LAST CHANGE MADE: " + LAST_CHANGE_DATE);
         Start.main(args);
     }
 }
