532e6e8b07325dcd840dfb0ca929e62289b06f3a
FilSos <filip.sosnowski88@gmail.com>
2019-12-20 20:26:59 +0100

    -ExcelExport order fix

--- src/main/java/converter/ExcelExport.java
+++ src/main/java/converter/ExcelExport.java
@@ -42,12 +42,12 @@ public class ExcelExport {
                 List<FragmentValidationResult> fragmentValidationResults = data.getFragmentValidationResults();
                 String fragments = fragmentValidationResults.stream().map(fragmentValidationResult -> fragmentValidationResult.getFragment().getQueryFragment())
                         .collect(Collectors.joining(", "));
-                String overlapCoefficient = fragmentValidationResults.stream().map(fragmentValidationResult -> String.valueOf(Math.floor(fragmentValidationResult.getOverlapCoefficient()*100)/100))
+                String overlapCoefficient = fragmentValidationResults.stream().map(fragmentValidationResult -> String.valueOf(Math.floor(fragmentValidationResult.getOverlapCoefficient() * 100) / 100))
                         .collect(Collectors.joining(", "));
-                String jaroWinklerSimilarity = fragmentValidationResults.stream().map(fragmentValidationResult -> String.valueOf(Math.floor(fragmentValidationResult.getJaroWinklerSimilarity()*100)/100))
+                String jaroWinklerSimilarity = fragmentValidationResults.stream().map(fragmentValidationResult -> String.valueOf(Math.floor(fragmentValidationResult.getJaroWinklerSimilarity() * 100) / 100))
                         .collect(Collectors.joining(", "));
                 Object[][] row = {{data.getIdentifier(), String.valueOf(data.getExNumber()), data.getQueryString(), data.isValid() ? "Tak" : "Nie",
-                        String.valueOf(data.getMatchedColumns()), String.valueOf(data.getMatchedTables()), String.valueOf(data.getResultMatchScore()), fragments, jaroWinklerSimilarity, overlapCoefficient,
+                        String.valueOf(data.getMatchedColumns()), String.valueOf(data.getMatchedTables()), String.valueOf(data.getResultMatchScore()), fragments, overlapCoefficient, jaroWinklerSimilarity,
                         String.valueOf(data.getTypos()), String.valueOf(data.getScore())}};
                 dataRows.add(row);
             } else {
