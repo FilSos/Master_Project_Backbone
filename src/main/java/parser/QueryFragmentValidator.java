8539d2cc1f8e3b3eb3590e143ad0f32828536e4a
Kheldar <psendrowski@gmail.com>
2019-12-20 19:33:03 +0100

    # updated algorithm for calculating overlap coefficient

--- src/main/java/parser/QueryFragmentValidator.java
+++ src/main/java/parser/QueryFragmentValidator.java
@@ -7,12 +7,20 @@ import org.apache.commons.text.similarity.IntersectionSimilarity;
 import org.apache.commons.text.similarity.JaroWinklerSimilarity;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
 
 public class QueryFragmentValidator {
+
     private JaroWinklerSimilarity jaroWinklerSimilarity = new JaroWinklerSimilarity();
-    private IntersectionSimilarity<String> intersectionSimilarity = new IntersectionSimilarity<>(cs -> Arrays.asList(cs.toString().toLowerCase().trim().split("\\s")));
+    private IntersectionSimilarity<String> intersectionSimilarity;
+
+    private static final List<String> tabooList = List.of(",", "\\.", ";");
+
 
     public FragmentValidationResult checkSimilarity(CodeFragment fragment, String query) {
+        intersectionSimilarity = new IntersectionSimilarity<>(charSequenceCollectionFunction);
         Double jaroWinkler = jaroWinklerSimilarity.apply(fragment.getQueryFragment().toLowerCase(), query);
         IntersectionResult intersectionResult = intersectionSimilarity.apply(fragment.getQueryFragment().toLowerCase(), query);
         double overlapCoefficient = calculateOverlapCoefficient(intersectionResult);
@@ -27,4 +35,15 @@ public class QueryFragmentValidator {
         }
         return (double) intersection / intersectionResult.getSizeB();
     }
+
+    Function<CharSequence, Collection<String>> charSequenceCollectionFunction = cs -> {
+        String initialString = cs.toString().toLowerCase().trim();
+
+        for (String tabooKey : tabooList) {
+            initialString = initialString.replaceAll(tabooKey, " ");
+        }
+
+        return Arrays.asList(initialString.split("\\s"));
+    };
+
 }
--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -5,8 +5,8 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
 
-    protected static final String CURRENT_VERSION = "0.93";
-    protected static final String LAST_CHANGE_DATE = "19.12.2019 14:11";
+    protected static final String CURRENT_VERSION = "0.94";
+    protected static final String LAST_CHANGE_DATE = "20.12.2019 19:32";
     private static Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
